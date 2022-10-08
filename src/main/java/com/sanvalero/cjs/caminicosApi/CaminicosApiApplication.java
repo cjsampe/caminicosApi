c4efd30fd51d7578dfcd7ef14d3f4dc0ebdc3bb2
cjsampe <samperiz@hotmail.com>
2022-10-08 23:42:06 +0200

    primeros cambios

---
+++ src/main/java/com/sanvalero/cjs/caminicosApi/CaminicosApiApplication.java
@@ -0,0 +1,13 @@
+package com.sanvalero.cjs.caminicosApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaminicosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaminicosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanvalero/cjs/caminicosApi/CaminicosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanvalero.cjs.caminicosApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaminicosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
